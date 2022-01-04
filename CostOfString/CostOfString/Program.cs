83e67fa1d1f7c6ad8d34743d5553e820264dc0c4
Seth Sides <sethsides@live.com>
2022-01-04 11:11:11 -0600

    added some comments

--- CostOfString/CostOfString/Program.cs
+++ CostOfString/CostOfString/Program.cs
@@ -10,12 +10,17 @@ namespace CostOfString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Type any word to get the value");
+            Console.WriteLine("Type any word to get the value.");
 
+            //user input for word you want the value of
             var inputWord = Console.ReadLine();
 
             var cost = CostOfString(inputWord);
+
+            //displays the user input and the "cost" of the given word.
             Console.WriteLine($"Cost of \"{inputWord}\" is : {cost}");
+
+
             Console.ReadKey();
         }
 
@@ -25,17 +30,20 @@ namespace CostOfString
 
             foreach (var c in s)
             {
+                //checks to see if each character in the given string is a vowel
                 if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
                 {
+                    //if the character is a vowel, the total is incremented by 2
                     total += 2;
                 }
                 else
                 {
+                    //if the character is a vowel, the total is incremented by 1
                     total += 1;
                 }
             }
 
-            return total;
+            return total; //returns the sum of the value of the vowels and consonants
         }
     }
 }
